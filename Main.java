a5b1f5ade24d068fcebdc25d641fb8fe4b47caa4
Blackadder008 <45468352+Blackadder008@users.noreply.github.com>
2018-11-30 09:26:17 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,17 +2,78 @@ import java.util.Scanner;
 
 public class Main {
 	
-	/* Constante */
+	/*Constante*/
+	
+	private static final String AJUDA = "ajuda", TERMINA = "termina", REGISTA = "regista", ENTRADA = "entrada";
+	private static final String SAI = "sai", NOVA = "nova", LISTA = "lista", BOLEIA = "boleia", CONSULTA = "consulta", REMOVE = "remove";
 	
 	private static String prompt = "> ";
 	
-	private static void changePrompt(boolean sessionState) {
-		if(sessionState)
+	/*Variaveis de Instancia*/
+	
+	private static boolean termina = false;
+	
+		/*Corpo da Classe*/
+	
+	/*Metodos Auxiliares*/
+
+	private static boolean isSessionActive() {
+		SessionState St1 = new SessionState()
+				
+				boolean cond = false;
+		
+		if(St1.isSessionActive)
+			cond = true;
+		
+		return cond;
+	}
+	
+	private static void changePrompt() {
+		if(isSessionActive())
 			System.out.print(+ prompt);
 		
 		else
 			System.out.print(prompt);
 		
+	}
+	
+	private static void unknownCommand() {
+		System.out.println("Comando inexistente.");
+	}
+	
+	
+	/*Metodos Principais*/
+	
+	/** Informa sobre os comandos disponiveis no programa */
+	
+	private static void processAjuda() {
+		if(!isSessionActive()) {
+			System.out.println("ajuda - Mostra os comandos existentes");
+			System.out.println("termina - Termina a execucao do programa");
+			System.out.println("regista - Regista um novo utilizador no programa");
+			System.out.println("entrada - Permite a entrada (”login”) dum utilizador no programa");
+		}
+		else {
+			System.out.println("ajuda - Mostra os comandos existentes");
+			System.out.println("sai - Termina a sessao deste utilizador no programa");
+			System.out.println("nova - Regista uma nova deslocacao");
+			System.out.println("lista - Lista todas ou algumas deslocacoes registadas");
+			System.out.println("boleia - Regista uma boleia para uma dada deslocacao");
+			System.out.println("consulta - Lista a informacao de uma dada deslocacao");
+			System.out.println("remove - Retira uma dada deslocacao");
+		}
+	}
+	
+	/** Termina a execucao do programa */
+	
+	private static void processTermina() {
+		if(isSessionActive()) {
+			termina = true;
+			System.out.println("Obrigado. Ate a proxima.");
+		}
+		else {
+			unknownCommand();
+		}
 	}
 	
 	/** Regista um novo utilizador no programa:
@@ -147,6 +208,60 @@ public class Main {
 		}
 		
 	}
+	
+	private static String readOption(Scanner in){
+		String opt = in.next().toLowerCase();
+		
+		return opt;
+	}
+	
+	private static void executeOption(String opt) {
+		if(!isSessionActive()) {
+			switch(opt) {
+			case AJUDA: processAjuda();
+			break;
+			
+			case TERMINA: processTermina();
+			break;
+			
+			case REGISTA: processRegista();
+			break;
+			
+			case ENTRADA: processEntrada();
+			break;
+			
+			default: unknownCommand();
+			break;
+			}
+		}
+		else{
+			switch(opt) {
+			case AJUDA: processAjuda();
+			break;
+			
+			case SAI: processSai();
+			break;
+			
+			case NOVA: processNova();
+			break;
+			
+			case LISTA: processLista();
+			break;
+			
+			case BOLEIA: processBoleia();
+			break;
+			
+			case CONSULTA: processConsulta();
+			break;
+			
+			case REMOVE: processRemove();
+			break;
+			
+			default: unknownCommand();
+			break;
+			}
+		}
+	}
 			
 	public static void main(String[] args) {
 	
@@ -154,6 +269,14 @@ public class Main {
 		
 		MainInteraction mi1 = new MainInteraction();
 		
+		String opt = "";
+		
+		do {
+			opt = readOption(in);
+			
+			executeOption(in);
+		}
+		while(!termina);
 	}
 
 }
