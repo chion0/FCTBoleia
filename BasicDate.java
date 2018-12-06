af6c6691fba6c4592fd1d7d7f557ae4364d48829
Blackadder008 <45468352+Blackadder008@users.noreply.github.com>
2018-12-06 22:13:50 +0000

    Update BasicDate.java

--- BasicDate.java
+++ BasicDate.java
@@ -1,3 +1,8 @@
+/**
+ * @author David Mira_d.mira
+ * @author Miguel Brites_m.brites
+ */
+
 public class BasicDate {
 
 	private static final int NUM_FIELDS = 3;
@@ -18,15 +23,17 @@ public class BasicDate {
 		}
 
 	}
-
+	
+/**Verifies if the day field, from a date, meets the required criteria to be valid,
+*meaning it checks if the value of the day field corresponds to a real world day.
+*/ 
 	private boolean isDayValid() {
 		
-		// Ano bissexto
 				boolean result = false;
-
-				if(getYear() % 4 == 0) {
+				
+				if(getYear() % 4 == 0) {//verifies if the year is a leap year 
 					if(getMonth() == 2) {
-						if(1 <= getDay() && getDay() <= 29) {
+						if(1 <= getDay() && getDay() <= 29) {//if the year is a leap year, the second month (february) has 29 days
 							result = true;	
 						}
 					}
@@ -60,6 +67,8 @@ public class BasicDate {
 				
 	}
 	
+/**Verifies if a date is valid by evaluating if its day and month fields correspond to real world values.
+*/
 	public boolean isValid() {
 		
 		boolean isMonthValid = false, result = false;
